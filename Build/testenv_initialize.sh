36a22f93dc1b289b89e20a072f78e2ea9d1bb3b7
Stanislav Muhametsin <stanislav.muhametsin@kapsi.fi>
2018-12-23 20:31:32 +0200

    Better container log printing.

--- Build/testenv_initialize.sh
+++ Build/testenv_initialize.sh
@@ -12,12 +12,12 @@ PG_SSL_CRT="${STATE_DIR}/pg_ssl_crt"
 STATE_DIR="$(realpath --relative-to="." "$STATE_DIR")"
 
 # We must first create all required containers...
-docker create --rm --expose 4222 --name cbam_test_nats --cidfile "${STATE_DIR}/cid_nats" nats:1.3.0-linux
-docker create --rm --expose 5432 --name cbam_test_pgsql -e POSTGRES_PASSWORD=postgres --cidfile "${STATE_DIR}/cid_pgsql" postgres:11.1-alpine
+docker create --expose 4222 --name cbam_test_nats --cidfile "${STATE_DIR}/cid_nats" nats:1.3.0-linux
+docker create --expose 5432 --name cbam_test_pgsql -e POSTGRES_PASSWORD=postgres --cidfile "${STATE_DIR}/cid_pgsql" postgres:11.1-alpine
 openssl req -newkey rsa:4096 -nodes -keyout "${PG_SSL_KEY}" -x509 -days 9999 -out "${PG_SSL_CRT}" -subj "/CN=cbam_test_pgsql_ssl"
 chmod u=rw,g=r,o= "${PG_SSL_KEY}"
 chmod u=rw,g=r,o= "${PG_SSL_CRT}"
-docker create --rm --expose 5432 --name cbam_test_pgsql_ssl -e POSTGRES_PASSWORD=postgres -v "${PG_SSL_KEY}:/etc/postgresql/server.key:ro" -v "${PG_SSL_CRT}:/etc/postgresql/server.crt:ro" -v "${SCRIPTDIR}/postgresql.conf.ssl:/etc/postgresql/postgresql.conf:ro" -v /etc/passwd:/etc/passwd:ro --cidfile "${STATE_DIR}/cid_pgsql_ssl" --user "$(id -u):$(id -g)" postgres:11.1-alpine -c 'config_file=/etc/postgresql/postgresql.conf'
+docker create --expose 5432 --name cbam_test_pgsql_ssl -e POSTGRES_PASSWORD=postgres -v "${PG_SSL_KEY}:/etc/postgresql/server.key:ro" -v "${PG_SSL_CRT}:/etc/postgresql/server.crt:ro" -v "${SCRIPTDIR}/postgresql.conf.ssl:/etc/postgresql/postgresql.conf:ro" -v /etc/passwd:/etc/passwd:ro --cidfile "${STATE_DIR}/cid_pgsql_ssl" --user "$(id -u):$(id -g)" postgres:11.1-alpine -c 'config_file=/etc/postgresql/postgresql.conf'
 # TODO create nginx/apache/other webserver so the CBAM HTTP tests won't need to use google.com
 
 # Create common network
@@ -30,5 +30,4 @@ find "${STATE_DIR}" -mindepth 1 -maxdepth 1 -type f -name 'cid_*' -exec sh -c 'd
 find "${STATE_DIR}" -mindepth 1 -maxdepth 1 -type f -name 'cid_*' -exec sh -c 'docker start $(cat "{}")' \;
 
 # Wait till all endpoints respond
-"${SCRIPTDIR}/wait-for.sh" -t 60 cbam_test_nats:4222 cbam_test_pgsql:5432
-"${SCRIPTDIR}/wait-for.sh" -t 60 cbam_test_pgsql_ssl:5432 || docker logs cbam_test_pgsql_ssl
+"${SCRIPTDIR}/wait-for.sh" -t 60 cbam_test_nats:4222 cbam_test_pgsql:5432 cbam_test_pgsql_ssl:5432
