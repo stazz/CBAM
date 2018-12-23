e6d10af322e9b0924c3237668bafab7a3a912624
Stanislav Muhametsin <stanislav.muhametsin@kapsi.fi>
2018-12-23 23:10:24 +0200

    PgSQL SCRAM container now works, but the authentication fails, debugging that.

--- Source/Tests/Tests.CBAM.SQL.PostgreSQL.Implementation/SCRAMTest.cs
+++ Source/Tests/Tests.CBAM.SQL.PostgreSQL.Implementation/SCRAMTest.cs
@@ -36,11 +36,15 @@ namespace Tests.CBAM.SQL.PostgreSQL.Implementation
          Assert.IsNotNull( saslDigest, "SASL byte digest must've been seen." );
          Assert.IsTrue( saslCalled, "SASL callback must've been called" );
 
+         Console.WriteLine( "First SCRAM Authentication successful." );
+
          // Now do test with just sasl digest
          creationInfo.CreationData.Initialization.Authentication.Password = null;
          creationInfo.CreationData.Initialization.Authentication.PasswordDigest = saslDigest;
          selectResult = await GetPool( creationInfo ).UseResourceAsync( async conn => { return await conn.GetFirstOrDefaultAsync<Int32>( "SELECT 1" ); } );
          Assert.AreEqual( 1, selectResult );
+
+         Console.WriteLine( "Second SCRAM Authentication successful." );
       }
 
 
